48ffe4b9888c41bd44164417c74c9d612b9c9de0
Sebas <sebastian.castellanos2012@gmail.com>
2023-05-28 17:10:48 -0500

    Cambios en LibroApp

--- src/componentes/FormularioLibro.jsx
+++ src/componentes/FormularioLibro.jsx
@@ -44,28 +44,7 @@ export const FormularioLibro = ({ agregar }) => {
             
                 
 
-                <nav class="navbar navbar-expand-lg bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">NAVEGADOR</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Libros</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Rentas</a>
-                                </li>
-                                
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                
             
             <div>
                 <form onSubmit={guardarLibro} style={{ backgroundColor: "#AD9978" }}>
